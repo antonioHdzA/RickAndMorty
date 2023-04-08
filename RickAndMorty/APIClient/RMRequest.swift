45910d6497a788cde003301533f9166e0677a3b2
Antonio Hernández Ambrocio <antonio.hdza12@gmail.com>
2023-04-08 11:42:52 -0600

    Create api request

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -9,5 +9,60 @@ import Foundation
 
 final class RMRequest {
 
-    
+    /// API Constants
+    private struct Constants {
+        static let baseURL = "https://rickandmortyapi.com/api"
+    }
+
+    /// Desired endpoint
+    private let endpoint: RMEndpoint
+
+    /// PathComponents for API, if any
+    private let pathComponents: [String]
+
+    /// Query arguments for API, in any
+    private let queryParameters: [URLQueryItem]
+
+    /// Constructed url for the API request in string format
+    private var urlString: String {
+        var string = Constants.baseURL
+        string += "/"
+        string += endpoint.rawValue
+
+        if !pathComponents.isEmpty {
+            pathComponents.forEach {
+                string += "/\($0)"
+            }
+        }
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap {
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }.joined(separator: "&")
+
+            string += argumentString
+        }
+
+        return string
+    }
+
+    /// Computed & constructed API url
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+
+    /// Desired http method
+    public let httpMethod = "GET"
+
+    /// Construct request
+    /// - Parameters:
+    ///   - endpoint: Target endpoint
+    ///   - pathComponents: Collection of path components
+    ///   - queryParameters: Collection of query parameters
+    public init(endpoint: RMEndpoint, pathComponents: [String] = [], queryParameters: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
